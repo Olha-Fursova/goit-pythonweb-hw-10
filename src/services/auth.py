c3d134fe879cc6078abb342d64f1a70b5622a654
Olha Fursova <teor15manypeople1@gmail.com>
2026-05-17 16:01:49 +0200

    Add operation for decoding access token in services/auth.py

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, UTC
 
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
 from src.conf.config import settings
@@ -35,3 +35,15 @@ def create_access_token(data: dict):
         SECRET_KEY,
         algorithm=ALGORITHM
     )
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+        return payload.get("sub")
+
+    except JWTError:
+        return None
