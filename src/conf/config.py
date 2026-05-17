05ddd5df76a182cc2812e7df429f74d245470ad1
Olha Fursova <teor15manypeople1@gmail.com>
2026-05-17 14:23:38 +0200

    Change env.py in migrations, Change config.py to a safer version, with links to .env that contains secrets and sensative data

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,4 +1,21 @@
-class Config:
-    DB_URL = "postgresql+asyncpg://postgres:567234@localhost:5432/contacts_db_hw10"
+from dotenv import load_dotenv
+import os
 
-config = Config()
+load_dotenv()
+
+
+class Settings:
+    DB_URL = os.getenv("DB_URL")
+
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    CLOUDINARY_NAME = os.getenv("CLOUDINARY_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
+
+
+settings = Settings()
